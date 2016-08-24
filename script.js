181df81fdae0689af147e9cbf806a4e09aba952f
mattfoster1 <matt@mfoster.co.uk>
2016-08-24 15:44:13 +0100

    first subtitle completed

---
+++ script.js
@@ -0,0 +1,8 @@
+var animate0 = function() {
+	//set this on a loop when timings known
+	var zero_line0 = document.getElementbyId("0line0");
+	var zero_line1 = document.getElementbyId("0line1");
+	var zero_line2 = document.getElementbyId("0line2");
+
+	
+}
