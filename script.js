865df2d9a5db5983df6930c8af280abf9392d68c
mattfoster1 <matt@mfoster.co.uk>
2016-08-25 16:33:03 +0100

    third project added

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-var animate0 = function() {
+var animate2 = function() {
 	//set this on a loop when timings known
-	var zero_line0 = document.getElementbyId("0line0");
-	var zero_line1 = document.getElementbyId("0line1");
-	var zero_line2 = document.getElementbyId("0line2");
-
-	
+	var line0 = document.getElementbyId("two_line0");
+	var line1 = document.getElementbyId("two_line1");
+	var line2 = document.getElementbyId("two_line2");
 }
